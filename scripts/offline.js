d4ddbc67d4a0769c379ab2e5fada96ca5b6e128a
Sidney Trzepacz <sidtrzisyourleader@gmail.com>
2022-06-12 15:45:41 -0400

    Fix problems with no internet and the browser

--- scripts/offline.js
+++ scripts/offline.js
@@ -1,6 +1,6 @@
 function offline() {
     console.warn("Offline");
-    document.body.innerHTML = '<h1>This page requires an internet connection</h1><button onclick="window.location.reload()">Try again</button> <button onclick="window.location.href = `../settings.html`">Back</button>';
+    document.body.innerHTML = '<h1>This page requires an internet connection</h1><button onclick="window.location.reload()">Try again</button> <button onclick="window.location.href = `../settings.html`">Connect</button>';
 }
 window.addEventListener('offline', offline);
 window.onload = () => {
