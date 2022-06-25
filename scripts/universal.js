2b3f7307e7f78b654e49c5522027b6c3d6899617
Sidney Trzepacz <sidtrzisyourleader@gmail.com>
2022-06-25 17:38:29 -0400

    Add Matomo Analytics

--- scripts/universal.js
+++ scripts/universal.js
@@ -263,3 +263,16 @@ BetterBoard.init({
     capsLockActive: false,
 });
 BetterBoard.run('input[type=text], input[type=number], textarea');
+<!-- Matomo -->
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+    var u="https://vestal.tk/matomo/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
+<!-- End Matomo Code -->
